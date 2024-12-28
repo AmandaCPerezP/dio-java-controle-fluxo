3b3e3a1ccc221b027049a6b1fbbea3d6169dc9db
Amanda Caroline Perez Pinto <amandacperezp@gmail.com>
2024-12-28 11:10:00 -0300

    condicional ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
-
-        if(nota >= 7)
-            System.out.println("Aprovado");
-
-            else if(nota >= 5 && nota < 7) // tem que sempre retornar true ou false
-            System.out.println("Prova de Recuperação");
-
-            else
-            System.out.println("Reprovado");
+       int nota = 5;
+       String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
     }
 }
